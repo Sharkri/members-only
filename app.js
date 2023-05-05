d0c949a9d5c6c94756a2863b70225982cd4e12b7
Sharkri <sharkdude955@gmail.com>
2023-05-05 09:50:45 -0500

    Add LocalStrategy

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const helmet = require("helmet");
 const compression = require("compression");
+const session = require("express-session");
+const passport = require("passport");
+const { LocalStrategy } = require("passport-local");
 const RateLimit = require("express-rate-limit");
 
 const limiter = RateLimit({
@@ -16,6 +20,7 @@ const limiter = RateLimit({
 });
 
 const indexRouter = require("./routes/index");
+const User = require("./models/User");
 
 const app = express();
 
@@ -28,6 +33,45 @@ main().catch((err) => console.error(err));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+app.use(
+  session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  // eslint-disable-next-line consistent-return
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      // case insensitive search
+      const user = await User.findOne({ username })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        // error occurred
+        if (err) {
+          return done(null, user);
+        }
+
+        if (res) {
+          // passwords match, log user in
+          return done(null, user);
+        }
+
+        // passwords do not match
+        return done(null, false, { message: "Incorrect password" });
+      });
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
 
 app.use(helmet());
 app.use(limiter);
