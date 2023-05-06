3c2010b6ecfcf7234bfbd6720a58fd506b893dbc
Sharkri <sharkdude955@gmail.com>
2023-05-06 11:52:04 -0500

    Add become an admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,22 @@
 const asyncHandler = require("express-async-handler");
 
-const memberDetails = {
-  title: "Become a Member",
-  description:
-    "By becoming a member, you will be able to see the author and date of each message.",
+const details = {
+  member: {
+    title: "Become a Member",
+    description:
+      "By becoming a member, you will be able to post messages and see the author and date of each message.",
+  },
+
+  admin: {
+    title: "Become an Admin",
+    description: "By becoming an admin, you will be able to delete messages.",
+  },
 };
 
 exports.becomeMemberGET = asyncHandler(async (req, res, next) => {
   if (!req.user) res.redirect("/login");
   else {
-    res.render("update-membership", memberDetails);
+    res.render("update-membership", details.member);
   }
 });
 
@@ -17,7 +24,7 @@ exports.becomeMemberPOST = asyncHandler(async (req, res, next) => {
   if (req.body.password !== process.env.MEMBER_PASSWORD) {
     // re-render form with password error
     res.render("update-membership", {
-      ...memberDetails,
+      ...details.member,
       errors: { password: { msg: "Incorrect member password" } },
       password: req.body.password,
     });
@@ -28,6 +35,34 @@ exports.becomeMemberPOST = asyncHandler(async (req, res, next) => {
     req.user.membershipStatus = "member";
     await req.user.save();
 
+    res.redirect("/");
+  } else {
+    const err = new Error("User does not exist");
+    err.status = 400;
+    next(err);
+  }
+});
+
+exports.becomeAdminGET = asyncHandler(async (req, res, next) => {
+  if (!req.user) res.redirect("/login");
+  else {
+    res.render("update-membership", details.admin);
+  }
+});
+
+exports.becomeAdminPOST = asyncHandler(async (req, res, next) => {
+  if (req.body.password !== process.env.ADMIN_PASSWORD) {
+    res.render("update-membership", {
+      ...details.admin,
+      errors: { password: { msg: "Incorrect admin password" } },
+      password: req.body.password,
+    });
+    return;
+  }
+
+  if (req.user) {
+    req.user.membershipStatus = "admin";
+    await req.user.save();
     res.redirect("/");
   } else {
     const err = new Error("User does not exist");
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,16 @@ const express = require("express");
 const {
   becomeMemberGET,
   becomeMemberPOST,
+  becomeAdminGET,
+  becomeAdminPOST,
 } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/become-a-member", becomeMemberGET);
 router.post("/become-a-member", becomeMemberPOST);
+
+router.get("/become-an-admin", becomeAdminGET);
+router.post("/become-an-admin", becomeAdminPOST);
 
 module.exports = router;
