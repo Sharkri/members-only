1d3caa5d51974819c529208430cbd4810b9cdee1
Sharkri <sharkdude955@gmail.com>
2023-05-06 13:10:53 -0500

    Add new message functionality

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,12 +1,58 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const Message = require("../models/Message");
 
 exports.newMessageGET = asyncHandler(async (req, res, next) => {
-  res.render("new-message", {
-    title: "New Message",
-    message: {},
-  });
+  if (!req.user) {
+    res.redirect("/login");
+  } else {
+    res.render("new-message", {
+      title: "New Message",
+      message: {},
+      errors: {},
+    });
+  }
 });
 
-exports.newMessagePOST = asyncHandler(async (req, res, next) => {
-  res.send("TODO: Implement new message POST");
-});
+exports.newMessagePOST = [
+  body("title")
+    .trim()
+    .isString()
+    .isLength({ min: 1 })
+    .withMessage("Title is required"),
+  body("text")
+    .trim()
+    .isString()
+    .isLength({ min: 1 })
+    .withMessage("Message text is required"),
+
+  asyncHandler(async (req, res, next) => {
+    if (!req.user) {
+      const err = new Error("User not logged in");
+      err.status = 400;
+      next(err);
+
+      return;
+    }
+
+    const errors = validationResult(req);
+
+    const message = new Message({
+      title: req.body.title,
+      text: req.body.text,
+      author: req.user.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("new-message", {
+        title: "New Message",
+        message,
+        errors: errors.mapped(),
+      });
+    } else {
+      await message.save();
+
+      res.redirect("/");
+    }
+  }),
+];
