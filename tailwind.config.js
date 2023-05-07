b2f23b09913ed19fdd07803021cb6e6070f18188
Sharkri <sharkdude955@gmail.com>
2023-05-06 20:52:45 -0500

    Add media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./views/**/*.pug"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "728px",
+      lg: "903px",
+      xlg: "1023px",
+    },
     extend: {},
   },
   plugins: [
